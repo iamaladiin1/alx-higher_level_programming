48a976ba5b7acdd3fb8cf806019bca2993971820
wasiu <adebayoabdulwasiu43@gmail.com>
2023-08-11 10:19:35 -0700

    7th

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    file = dir(hidden_4)
+    length = len(file)
+    for l in range(0, string):
+        if file[l][0:2] != "__":
+            print(file[l])
